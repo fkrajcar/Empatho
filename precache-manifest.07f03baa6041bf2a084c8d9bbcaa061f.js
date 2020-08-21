312d565551f669917bb846251d522d1835bd9d58
fkrajcar <filip.krajcar@asseco-see.hr>
2020-08-21 19:12:08 +0200

    Updates

---
+++ precache-manifest.07f03baa6041bf2a084c8d9bbcaa061f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8e06940641e63b962fdec847a24599c",
+    "url": "/Empatho/index.html"
+  },
+  {
+    "revision": "a0a4ecb9967eecb5ed27",
+    "url": "/Empatho/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5f2da06be51619812766",
+    "url": "/Empatho/static/css/main.752c2b1a.chunk.css"
+  },
+  {
+    "revision": "a0a4ecb9967eecb5ed27",
+    "url": "/Empatho/static/js/2.0b5a5020.chunk.js"
+  },
+  {
+    "revision": "d0b59f24596445a3fbb9bf55a0c27bca",
+    "url": "/Empatho/static/js/2.0b5a5020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f2da06be51619812766",
+    "url": "/Empatho/static/js/main.57d4eb04.chunk.js"
+  },
+  {
+    "revision": "60441728d1818157230a",
+    "url": "/Empatho/static/js/runtime-main.c4a107da.js"
+  }
+]);
