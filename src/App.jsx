9a0dac314aa3c481b28815b63a622f61d2175955
fkrajcar <filip.krajcar@asseco-see.hr>
2020-08-22 22:23:58 +0200

    Added 60 second game timer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import Webcam from 'react-webcam';
 import Button from 'react-bootstrap/Button';
+import sample from 'lodash/sample';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -16,6 +17,7 @@ import {
   VIDEO_CONSTRAINTS,
   TIMER_INTERVAL_MS,
   IMAGES_URL,
+  EXPRESSIONS,
 } from './constants';
 
 export default class App extends Component {
@@ -31,8 +33,14 @@ export default class App extends Component {
       isDetected: false,
       hasStarted: false,
       detectedExpression: 'neutral',
+      randomExpression: sample(EXPRESSIONS),
       startingTimer: 3,
       intervalID: null,
+      gameTimerIntervalID: null,
+      correctEmotionTimerIntervalID: null,
+      gameCorrectEmotionTimer: 5,
+      gameTimer: 60,
+      gameScore: 0,
     };
   }
 
@@ -44,11 +52,6 @@ export default class App extends Component {
 
   componentDidUpdate(prevProps, prevState, snapshot) {
     const { hasStarted, startingTimer } = this.state;
-
-    if (hasStarted === true && startingTimer === 3) {
-
-    }
-    console.log(prevProps.startingTimer);
   }
 
     getFullFaceDescription = async (pictureSrc) => {
@@ -77,11 +80,51 @@ export default class App extends Component {
       await faceapi.loadFaceExpressionModel(MODEL_URL);
     };
 
+    gameLogicStart = () => {
+      let { gameTimerIntervalID,
+        correctEmotionTimerIntervalID,
+        gameCorrectEmotionTimer,
+        gameScore,
+        gameTimer,
+      } = this.state;
+
+      this.setState(
+        { gameScore: 0 },
+      );
+
+      gameTimerIntervalID = setInterval(() => {
+        gameTimer -= 1;
+        console.log(gameTimer);
+        this.setState(
+          { gameTimer },
+        );
+      }, TIMER_INTERVAL_MS);
+
+      this.setState(
+        { gameTimerIntervalID },
+      );
+    }
+
+  gameLogicStop = () => {
+    const { gameTimerIntervalID,
+      correctEmotionTimerIntervalID,
+      gameCorrectEmotionTimer,
+      gameScore,
+      gameTimer,
+    } = this.state;
+
+    clearInterval(gameTimerIntervalID);
+    this.setState(
+      { gameTimer: 60 },
+    );
+  }
+
   startGame = () => {
     const { hasStarted } = this.state;
     let { startingTimer, intervalID } = this.state;
 
     if (hasStarted) {
+      this.gameLogicStop();
       clearInterval(intervalID);
       this.setState({
         hasStarted: !hasStarted,
@@ -90,6 +133,7 @@ export default class App extends Component {
     } else {
       intervalID = setInterval(() => {
         if (startingTimer === 0) {
+          this.gameLogicStart();
           clearInterval(intervalID);
         } else {
           startingTimer -= 1;
@@ -114,8 +158,8 @@ export default class App extends Component {
     };
 
     render() {
-      const { isDetected, detectedExpression, isLoading, hasStarted, startingTimer } = this.state;
-      console.log(startingTimer);
+      const { isDetected, detectedExpression, isLoading, hasStarted, startingTimer, randomExpression } = this.state;
+
       return (
         <Container>
           <div className={hasStarted ? 'webcam-faces hasStarted' : 'webcam-faces'}>
@@ -137,7 +181,7 @@ export default class App extends Component {
               {
                 hasStarted && startingTimer !== 0
                   ? <p className="timer">{startingTimer}</p>
-                  : <Image className={hasStarted ? 'emoji hasStarted' : 'emoji'} src={`${IMAGES_URL}${detectedExpression}.png`} />
+                  : <Image className={hasStarted ? 'emoji hasStarted' : 'emoji'} src={`${IMAGES_URL}${randomExpression}.png`} />
                }
             </div>
           </div>
@@ -146,7 +190,7 @@ export default class App extends Component {
           </div>
           <div className="controls">
             <Button
-              variant="primary"
+              variant={hasStarted ? 'danger' : 'primary'}
               onClick={this.startGame}
             >
               {
--- src/constants/index.js
+++ src/constants/index.js
@@ -2,6 +2,12 @@ module.exports.WEBCAM_HEIGHT = 480;
 module.exports.WEBCAM_WIDTH = 480;
 module.exports.EXPRESSION_DETECTION_INTERVAL_MS = 250;
 module.exports.TIMER_INTERVAL_MS = 1000;
+module.exports.EXPRESSIONS_ANGRY = 'angry';
+module.exports.EXPRESSIONS_HAPPY = 'happy';
+module.exports.EXPRESSIONS_NEUTRAL = 'neutral';
+module.exports.EXPRESSIONS_SAD = 'sad';
+module.exports.EXPRESSIONS_SURPRISED = 'surprised';
+module.exports.EXPRESSIONS = [this.EXPRESSIONS_ANGRY, this.EXPRESSIONS_HAPPY, this.EXPRESSIONS_NEUTRAL, this.EXPRESSIONS_SAD, this.EXPRESSIONS_SURPRISED];
 
 module.exports.VIDEO_CONSTRAINTS = {
   width: this.WEBCAM_WIDTH,
